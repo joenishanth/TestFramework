1dbc0b11671811738405bb78c751b659202000ca
Joe Nishanth <joenishanth@gmail.com>
2020-09-19 18:37:30 +1000

    Add project files.

---
+++ TestProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomGenerator = new RandomGenerator();
+            var number = randomGenerator.RandomNumber(10000, 20000);
+            var randomString = randomGenerator.RandomString(100);
+            var password = randomGenerator.RandomPassword();
+
+            var alphaNumericString = randomGenerator.RandomString(100, "0123456789ABCDEFGHIJKLMNOPQSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+
+            Console.WriteLine(number);
+            Console.WriteLine(randomString);
+            Console.WriteLine(password);
+            Console.WriteLine("AlphaNumericString :{0}", alphaNumericString);
+        }
+    }
+}
